fcb3cfb312e8632611ef1799c590e7b7d482a8c3
Fanta-sia <kc08choi@gmail.com>
2025-03-08 10:38:17 -0800

    Added js variables and buttons for gallery

---
+++ gallery_script.js
@@ -0,0 +1,11 @@
+const imageGallery = [
+    "https://unsplash.com/photos/buildings-and-body-of-water-WHHY-iBp3aI",
+    "https://unsplash.com/photos/skyline-photography-of-boat-passing-on-waters-overlooking-buildings-2yN10KHNTzM",
+    "https://unsplash.com/photos/three-brown-wooden-boat-on-blue-lake-water-taken-at-daytime-T7K4aEPoGGk",
+    "https://unsplash.com/photos/pathway-in-forest-with-yellow-sunlight-F7HGqkkMYAU"
+]
+const imageElement = document.getElementById("gallery-image");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+let currentImgIndex = 0;
